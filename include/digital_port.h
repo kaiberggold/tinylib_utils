19ca3c86310b35c0eecfb4bfc2ed8ad1174d5114
Kai Berggold <mail@kaiberggold.de>
2024-03-01 22:51:13 +0000

    digital_port initially working

--- include/digital_port.h
+++ include/digital_port.h
@@ -7,6 +7,9 @@ namespace utils
     template<typename addr_t, typename reg_t, const std::uint8_t port_idx>
     struct DigitalPort
     {
-        static void set_port(reg_t val);
+        static void set_port(reg_t val)
+        {
+            tlm::dpo::McalDigitalPort<addr_t,reg_t,port_idx>::set_port(val);
+        }       
     };
 }
--- src/usart_dbg/digital_port.cpp
+++ src/usart_dbg/digital_port.cpp
@@ -1,9 +1,9 @@
-#include "digital_port.h"
-#include "mcal.h"
+// #include "digital_port.h"
+// #include "mcal.h"
 
 
-template<typename addr_t, typename reg_t, const std::uint8_t port_idx>
-void utils::DigitalPort<addr_t,reg_t,port_idx>::set_port(reg_t val) 
-{
-    tlm::dpo::McalDigitalPort<addr_t,reg_t,port_idx>::setport(reg_t::val);
-}
+// template<typename addr_t, typename reg_t, const std::uint8_t port_idx>
+// void utils::DigitalPort<addr_t,reg_t,port_idx>::set_port(reg_t val) 
+// {
+//     tlm::dpo::McalDigitalPort<addr_t,reg_t,port_idx>::setport(reg_t val);
+// }
