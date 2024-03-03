65a52b4f0d57219abba393fbe34c05c5a1b7e231
Kai Berggold <mail@kaiberggold.de>
2024-03-03 00:15:41 +0000

    formatter

--- src/digital_port.h
+++ src/digital_port.h
@@ -1,24 +1,18 @@
 #include <cstdint>
 #include <mcal.h>
 
+namespace utils {
+template <typename addr_t, typename reg_t, const std::uint8_t port_idx>
+struct DigitalPort {
+  static void set_port(reg_t val) {
+    hal::McalDigitalPort<addr_t, reg_t, port_idx>::set_reg(val);
+  }
+  static void get_port(reg_t val) {
+    hal::McalDigitalPort<addr_t, reg_t, port_idx>::get_reg(val);
+  }
+  static void set_to_out_mask(reg_t val) {
+    hal::McalDigitalPort<addr_t, reg_t, port_idx>::set_to_out_mask(val);
+  }
+};
 
-namespace utils
-{
-    template<typename addr_t, typename reg_t, const std::uint8_t port_idx>
-    struct DigitalPort
-    {
-        static void set_port(reg_t val)
-        {
-            hal::McalDigitalPort<addr_t,reg_t,port_idx>::set_reg(val);
-        }       
-        static void get_port(reg_t val)
-        {
-            hal::McalDigitalPort<addr_t,reg_t,port_idx>::get_reg(val);
-        }
-        static void set_to_out_mask(reg_t val)
-        {
-            hal::McalDigitalPort<addr_t,reg_t,port_idx>::set_to_out_mask(val);
-        }         
-    };
-
-}
+} // namespace utils
--- src/usart_dbg.h
+++ src/usart_dbg.h
@@ -1,46 +1,43 @@
 #ifndef USART_DBG
-  #define USART_DBG
+#define USART_DBG
 
-
-#include <cstdint>
-#include <array>
 #include "mcal_usart.h"
+#include <array>
+#include <cstdint>
 #include <cstring>
 
 #
-namespace utils{
-    class UsartDbg
-    {
-    private:
-        std::uint16_t baud_rate;
-        constexpr static std::uint8_t buffer_size =255;
-        std::array<std::uint8_t, buffer_size> _send_buffer;
-        std::uint8_t _pos_in;
-        std::uint8_t _pos_out;
-        std::uint8_t _n;
-        
-        void _ring_buffer_in(std::uint8_t data);
-        std::uint8_t _ring_buffer_out();
-        bool _ring_buffer_empty();
+namespace utils {
+class UsartDbg {
+private:
+  std::uint16_t baud_rate;
+  constexpr static std::uint8_t buffer_size = 255;
+  std::array<std::uint8_t, buffer_size> _send_buffer;
+  std::uint8_t _pos_in;
+  std::uint8_t _pos_out;
+  std::uint8_t _n;
 
-        bool _usart_buffer_empty();
-        hal::McalUsart mu;
+  void _ring_buffer_in(std::uint8_t data);
+  std::uint8_t _ring_buffer_out();
+  bool _ring_buffer_empty();
 
-    public:
-        UsartDbg(std::uint32_t baud_rate)
-        {
-            _pos_in=0;
-            _pos_out=0;
-            _n=0;
-            this->usart_dbg_init(baud_rate);
-        }
- 
-        template <typename T> void print_decimal(T value);
-        void print_text(const char text[]);
-        void print_ascii(std::uint8_t value);
-        void usart_dbg_step();
-        void usart_dbg_init(std::uint32_t baud_rate);
-        void usart_transmit_byte(std::uint8_t);     
-    };
-}
+  bool _usart_buffer_empty();
+  hal::McalUsart mu;
+
+public:
+  UsartDbg(std::uint32_t baud_rate) {
+    _pos_in = 0;
+    _pos_out = 0;
+    _n = 0;
+    this->usart_dbg_init(baud_rate);
+  }
+
+  template <typename T> void print_decimal(T value);
+  void print_text(const char text[]);
+  void print_ascii(std::uint8_t value);
+  void usart_dbg_step();
+  void usart_dbg_init(std::uint32_t baud_rate);
+  void usart_transmit_byte(std::uint8_t);
+};
+} // namespace utils
 #endif // USART_DBG
