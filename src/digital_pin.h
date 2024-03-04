14afb7c6e3b40156c82c54979c06eeef0264c411
Kai Berggold <mail@kaiberggold.de>
2024-03-04 19:40:16 +0000

    add new  files

---
+++ src/digital_pin.h
@@ -0,0 +1,27 @@
+#ifndef DIGITAL_PIN
+#define DIGITAL_PIN
+#include <cstdint>
+// #include <mcal.h>
+#include "digital_pin.h"
+namespace utils
+{
+    template <typename addr_t, typename reg_t, const std::uint8_t port_idx, std::uint8_t pin>
+    struct DigitalPin
+    {
+        static void set_pin(pin, reg_t val)
+        {
+            hal::McalDigitalPort<addr_t, reg_t, port_idx>::set_bits(val, pin);
+        }
+        // static void get_port(reg_t val)
+        // {
+        //     hal::McalDigitalPort<addr_t, reg_t, port_idx>::get_reg(val);
+        // }
+        static v set_to_out_pin(pin)
+        {
+            hal::McalDigitalPort<addr_t, reg_t, port_idx>::set_to_out_mask(val);
+        }
+    };
+
+} // namespace utils
+
+#endif
---
+++ src/i2c.h
@@ -0,0 +1,4 @@
+#ifndef USART_I2C
+#define USART_I2C
+
+#endif
