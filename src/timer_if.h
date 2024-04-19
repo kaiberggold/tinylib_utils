d48ad5ee21ffb7853f173af980f1542f68a2a20b
Kai Berggold <mail@kaiberggold.de>
2024-04-19 15:59:24 +0200

    Example code type traits

--- src/timer_if.h
+++ src/timer_if.h
@@ -1,3 +1,34 @@
+#include <iostream>
+#include <type_traits>
+
+// Base class using CRTP
+template <typename Derived>
+class Base {
+public:
+    template <typename T = Derived>
+    static typename std::enable_if<std::is_same_v<decltype(T::baseStaticFunction), void(*)()>>::type
+    baseStaticFunction() {
+        Derived::derivedStaticFunction();
+    }
+};
+
+// Derived class implementing the derived static function
+class Derived : public Base<Derived> {
+public:
+   //static void baseStaticFunction() {
+   //     std::cout << "Base static function called!" << std::endl;
+   // }
+
+
+};
+
+int main() {
+    Derived::baseStaticFunction(); // Output: "Derived static function called!"
+
+    return 0;
+}
+
+
 #ifndef TIMER_IF
 #define TIMER_IF
 #include <cstdint>
