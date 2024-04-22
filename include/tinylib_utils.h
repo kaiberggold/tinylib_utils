2e69b72a56008b24e167d297ab8b9e0bb37fa628
Kai Berggold <mail@kaiberggold.de>
2024-04-22 23:02:46 +0200

    timer_if

--- include/tinylib_utils.h
+++ include/tinylib_utils.h
@@ -9,7 +9,7 @@
 #include "mp44xx.h"
 #include "mpc3202.h"
 #include "digi_poti.h"
-#include "timer.h"
+#include "timer_if.h"
 #include "interrupts.h"
 #include "poti_ic_if.h"
 #include "ad_ic_static_if.h"
--- src/timer_if.h
+++ src/timer_if.h
@@ -1,50 +1,26 @@
-#include <iostream>
-#include <type_traits>
-
-// Base class using CRTP
-template <typename Derived>
-class Base {
-public:
-    template <typename T = Derived>
-    static typename std::enable_if<std::is_same_v<decltype(T::baseStaticFunction), void(*)()>>::type
-    baseStaticFunction() {
-        Derived::derivedStaticFunction();
-    }
-};
-
-// Derived class implementing the derived static function
-class Derived : public Base<Derived> {
-public:
-   //static void baseStaticFunction() {
-   //     std::cout << "Base static function called!" << std::endl;
-   // }
-
-
-};
-
-int main() {
-    Derived::baseStaticFunction(); // Output: "Derived static function called!"
-
-    return 0;
-}
-
-
 #ifndef TIMER_IF
 #define TIMER_IF
+
 #include <cstdint>
-namespace ifc
+#include <type_traits>
+
+namespace utils
 {
     template <typename TimerT, typename addr_t, typename reg_t, typename timer_t, reg_t timer_idx>
     struct TimerIf
     {
+
         static void init(timer_t start_time)
         {
+
             TimerT::init_impl(start_time);
         }
+
         static void irq_reset(timer_t start_time)
         {
             TimerT::irq_reset_impl(start_time);
         }
     };
 }
+
 #endif
